946957972a35bb13d5231379f6501e77926c5b90
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2023-07-05 01:31:02 +0630

    Add card effect and description expend when clicked

--- app/templates/test_template/app.js
+++ app/templates/test_template/app.js
@@ -1,22 +1,32 @@
-let items_tag = {
-    image: document.querySelectorAll("#item-src"),
-    name: document.querySelectorAll("#item-name"),
-    description: document.querySelectorAll("#description")
-};
-let items = {
-    item_name: "PhyoeKyawThan",
-    item_src: "./product.jpg",
-    description: "The best product for play with feeling and can leave alone when u getting bored."
-};
+const moreButtons = document.querySelectorAll(".more");
+let expandedCard = null;
 
-window.onload = (e)=>{
-    items_tag.image.forEach(img => {
-        img.src = items.item_src;
+moreButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const card = e.target.closest(".card");
+        if (card !== expandedCard) {
+            if (expandedCard) {
+                resetCard(expandedCard);
+            }
+            expandCard(card);
+            expandedCard = card;
+        } else {
+            resetCard(card);
+            expandedCard = null;
+        }
     });
-    items_tag.name.forEach(name =>{
-        name.textContent = items.item_name;
-    });
-    items_tag.description.forEach(des =>{
-        des.textContent = items.description;
-    });
-};
+});
+
+function expandCard(card) {
+    card.style.height = "400px";
+    card.querySelector(".option").style.margin = "53px auto";
+    card.querySelector(".description").style.overflow = "scroll";
+    card.querySelector(".description").style.maxHeight = "100px";
+}
+
+function resetCard(card) {
+    card.style.height = ""; // Resets the height to its default value
+    card.querySelector(".option").style.margin = "";
+    card.querySelector(".description").style.overflow = "";
+    card.querySelector(".description").style.maxHeight = "";
+}
